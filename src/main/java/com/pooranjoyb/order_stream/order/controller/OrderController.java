909cfeb72d0861f619f111aacad377fbf181336d
Pooranjoy <pooranjoyb2016@gmail.com>
2026-04-22 00:30:56 +0530

    #1 fear: create-order

--- src/main/java/com/pooranjoyb/order_stream/order/controller/OrderController.java
+++ src/main/java/com/pooranjoyb/order_stream/order/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.pooranjoyb.order_stream.order.controller;
 
+import com.pooranjoyb.order_stream.order.dto.OrderRequestDto;
+import com.pooranjoyb.order_stream.order.entity.Order;
 import com.pooranjoyb.order_stream.order.service.OrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/order")
 public class OrderController {
 
     @Autowired
@@ -14,5 +20,11 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody @Valid OrderRequestDto requestDto) {
+        Order order = orderService.createOrder(requestDto);
+        return ResponseEntity.ok(order);
     }
 }
