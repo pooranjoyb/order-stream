5740a51d2e0954a1f52f9b8a56152a8c04f5ca5a
Shreya pandey <shreyapandey37383@gmail.com>
2026-04-25 11:33:48 +0530

    Add RabbitMQ configuration for order processing

---
+++ src/main/java/com/pooranjoyb/order_stream/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package com.pooranjoyb.order_stream.config;  
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE = "order_queue";
+    public static final String EXCHANGE = "order_exchange";
+    public static final String ROUTING_KEY = "order_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
+        RabbitTemplate template = new RabbitTemplate(factory);
+        template.setMessageConverter(converter());
+        return template;
+    }
+}
