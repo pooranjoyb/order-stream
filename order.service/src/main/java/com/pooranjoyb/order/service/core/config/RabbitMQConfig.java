10afa35eeed160bb2b75abfba7551e4cd15ad157
itzzavdheshh <aavdhesh.dadhich@gmail.com>
2026-05-05 10:06:18 +0530

    feat: consume ProductReservationResultEvent and update order status

--- order.service/src/main/java/com/pooranjoyb/order/service/core/config/RabbitMQConfig.java
+++ order.service/src/main/java/com/pooranjoyb/order/service/core/config/RabbitMQConfig.java
@@ -1,5 +1,11 @@
 package com.pooranjoyb.order.service.core.config;
 
+import com.pooranjoyb.shared.messaging.ExchangeNames;
+import com.pooranjoyb.shared.messaging.RoutingKeys;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
@@ -8,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    public static final String PRODUCT_RESERVATION_RESULT_QUEUE = "product.reservation.result.queue";
 
     @Bean
     public JacksonJsonMessageConverter messageConverter() {
@@ -22,5 +30,26 @@ public class RabbitMQConfig {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public Queue productReservationResultQueue() {
+        return new Queue(PRODUCT_RESERVATION_RESULT_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(ExchangeNames.ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding productReservationResultBinding(
+            Queue productReservationResultQueue,
+            TopicExchange orderExchange
+    ) {
+        return BindingBuilder
+                .bind(productReservationResultQueue)
+                .to(orderExchange)
+                .with(RoutingKeys.PRODUCT_RESERVED);
     }
 }
